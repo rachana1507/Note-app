6753b616adb0b783454a927aa183aca3c7e1f4b2
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-07-25 09:54:00 +0530

    Prepared for the deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes} from "react-router"
-import HomePage from './pages/Homepage'
+import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
 import toast from 'react-hot-toast'
